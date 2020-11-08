993e2202529b6c920ec881c4914ceb51199d3ed1
Ernesto David Escariz <davidescariz13@gmail.com>
2020-11-08 10:36:57 -0500

    Setting up proyect

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="styles/bootstrap.css">
+</head>
+<body>
+      
+    
+    <h1 class="jumbotron">Homepage</h1>
+
+    
+
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+</body>
+</html>
